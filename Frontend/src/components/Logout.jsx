e50b80538860799f58f0d3b6e632ef5151ed0790
Mina <Mina@example.com>
2024-01-31 14:43:22 +0100

    logout first

--- Frontend/src/components/Logout.jsx
+++ Frontend/src/components/Logout.jsx
@@ -1,24 +1,37 @@
-import React from 'react';
-import  axios  from 'axios';
-import backendUrl from '../config/config';
+import React, { useState } from "react";
+import axios from "axios";
+import backendUrl from "../config/config";
 
 const Logout = () => {
-    const logoutHandler = async (e) => {
-        e.preventDefault();
-    
-        try {
-          const resp = await axios.post(`${backendUrl}/logout`, {}, { withCredentials: true });
-          console.log("you are logout!", resp.data);
-      
-        } catch (error) {
-          console.log(error.message);
-        }
+  const [showExit, setShowExit] = useState(false);
+  const logoutHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const resp = await axios.post(
+        `${backendUrl}/logout`,
+        {},
+        { withCredentials: true }
+      );
+  
+      if (resp.status === 200) {
+        console.log("You are logged out!", resp.data);
+        setShowExit(true);
+      } else {
+        console.log("Error while logging out");
       }
-    return (
-        <div>
-           <button onClick={logoutHandler}>LOGOUT</button>
-        </div>
-    );
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  
+  return (
+    <div>
+      {showExit && (
+        <div className='size-96 rounded-md bg-gradient-to-r from-blue-500 to-transparent '></div>
+      )}
+      <button onClick={logoutHandler}>LOGOUT</button>
+    </div>
+  );
 };
 
 export default Logout;
