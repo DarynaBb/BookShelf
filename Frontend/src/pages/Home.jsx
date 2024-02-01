8cd4bdb741f259ddd98dbbae2329a2fa7e7939a5
reza <reza@gmail.com>
2024-02-01 16:15:12 +0100

    searchBar updated

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -3,13 +3,24 @@ import SearchBar from "../components/SearchBar";
 import BookImage from "../assets/image-29.png";
 import MenuBar from "../components/MenuBar";
 import ContactBar from "../components/ContactBar";
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import SearchResultPage from "./SearchResultPage";
 
-const handleSearch = (query) => {
-  console.log("Suche nach:", query);
-};
+// const handleSearch = (query) => {
+//   console.log("Suche nach:", query);
+// };
 
 const Home = () => {
 
+  const navigate = useNavigate();
+  const [searchResults, setSearchResults] = useState([]);
+  const handleSearch = (results) => {
+    // Hier speicherst du die Suchergebnisse im Zustand
+    setSearchResults(results);
+    // Dann navigierst du zur SearchResultPage
+    navigate('/search');
+  };
   return (
     <div className="bg-[#ffeed94c]">
       <MenuBar />
