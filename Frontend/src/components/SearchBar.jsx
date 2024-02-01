47cc4c137fff235a1e0b7a95ca6f353770f38b1b
reza <reza@gmail.com>
2024-02-01 10:06:53 +0100

    searchPage(Beta) added

--- Frontend/src/components/SearchBar.jsx
+++ Frontend/src/components/SearchBar.jsx
@@ -7,57 +7,53 @@ const SearchBar = ({ onSearch }) => {
     setSearchQuery(e.target.value);
   };
 
-  const handleSearch = () => {
-    onSearch(searchQuery);
+  const handleSearch = async () => {
+    try {
+        const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=Harry+Potter`);
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      // Verarbeite die Daten hier, z.B. aktualisiere den Zustand oder rufe die onSearch-Funktion mit den Daten auf
+      onSearch(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
   };
 
   return (
     <div>
-      {/* <!-- Component: Plain large search input  --> */}
-      <div class="relative my-6">
+      {/* Komponente: Einfache große Sucheingabe */}
+      <div className="relative my-6">
         <input
-          id="id-l15"
           type="text"
-          name="id-l15"
           placeholder="Search by title, author or genre"
-          class="relative w-full h-12 px-4 pr-12 transition-all border-b outline-none focus-visible:outline-none peer border-slate-200 text-slate-500 autofill:bg-white invalid:border-pink-500 invalid:text-pink-500 focus:border-emerald-500 focus:outline-none invalid:focus:border-pink-500 disabled:cursor-not-allowed disabled:bg-slate-50 disabled:text-slate-400"
+          className="relative w-full h-12 px-4 pr-12 transition-all border-b outline-none focus-visible:outline-none peer border-slate-200 text-slate-500 autofill:bg-white invalid:border-pink-500 invalid:text-pink-500 focus:border-emerald-500 focus:outline-none invalid:focus:border-pink-500 disabled:cursor-not-allowed disabled:bg-slate-50 disabled:text-slate-400"
+          value={searchQuery}
+          onChange={handleSearchChange}
         />
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          class="absolute w-6 h-6 cursor-pointer top-3 right-4 stroke-slate-400 peer-disabled:cursor-not-allowed"
+          className="absolute w-6 h-6 cursor-pointer top-3 right-4 stroke-slate-400 peer-disabled:cursor-not-allowed"
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
           strokeWidth="1.5"
           aria-hidden="true"
-          aria-labelledby="title-11 description-11"
-          role="graphics-symbol"
           onClick={handleSearch}
         >
-          <title id="title-11">Search icon</title>
-          <desc id="description-11">Icon description here</desc>
+          <title>Search icon</title>
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
           />
         </svg>
       </div>
-      {/* <!-- End Plain large search input --> */}
+      {/* Ende Einfache große Sucheingabe */}
     </div>
   );
 };
 
-{
-  /* <input
-                type="text"
-                placeholder="Suche nach Büchern oder Genre..."
-                value={searchQuery}
-                onChange={handleSearchChange}
-                className="border border-gray-300 p-2 rounded-md w-84"
-            />
-            <button onClick={handleSearch} className="bg-blue-500 text-white px-4 py-2 rounded-md ml-2">
-                Suche
-            </button> */
-}
 export default SearchBar;
