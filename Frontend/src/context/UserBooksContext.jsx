74386dc4029fe3517a2b9e584aa841a4a185eb43
DarynaBb <darinabilenko@gmail.com>
2024-02-02 17:43:21 +0100

    styling for currently-books-section

--- Frontend/src/context/UserBooksContext.jsx
+++ Frontend/src/context/UserBooksContext.jsx
@@ -8,13 +8,17 @@ const UserBooksContextProvider = ({ children }) => {
     const [isCurrentlyReading, setIsCurrentlyReading] = useState(false);
     const [isWantToRead, setIsWantToRead] = useState(false);
     const [isRead, setIsRead] = useState(false);
-    const [isLoading, setIsLoading] = useState(false);
     const [currentlyReading, setCurrentlyReading] = useState([]);
     const [wantToRead, setWantToRead] = useState([]);
     const [read, setRead] = useState([]);
     const [isShelfUpdated, setIsShelfUpdated] = useState(false);
     const [isBookDeleted, setIsBookDeleted] = useState(false);
     const [isMyBooksOpen, setIsMyBooksOpen] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const [chosenCurrentlyBook, setChosenCurrentlyBook] = useState([]);
+    
+
+
     const url = "http://localhost:3001";
 
     const getBooks = async () => {
@@ -24,12 +28,14 @@ const UserBooksContextProvider = ({ children }) => {
           const wantToReadBooks = response.data.filter(book => book.shelfType === "wantToRead"); 
           const readBooks = response.data.filter(book => book.shelfType === "read");
           
+          
           setCurrentlyReading(currentlyReadingBooks);
           setWantToRead(wantToReadBooks);
           setRead(readBooks);
           setIsLoading(true);
           setIsShelfUpdated(false);
           setIsBookDeleted(false);
+          setIsLoading(true);
   
           console.log('Books:', response.data);
           console.log('Currently Reading Books:', currentlyReadingBooks);
@@ -47,6 +53,7 @@ const UserBooksContextProvider = ({ children }) => {
           await axios.patch(axiosUrl, {
             shelfType: event.target.value
           });
+          setChosenCurrentlyBook([]);
         } catch (error) {
           console.error('Error updating shelf type:', error.message);
         } 
@@ -76,7 +83,7 @@ const UserBooksContextProvider = ({ children }) => {
                 isShelfUpdated, setIsShelfUpdated,
                 isBookDeleted, setIsBookDeleted,
                 userId, changeShelf, deleteBook,
-                isMyBooksOpen, setIsMyBooksOpen, url
+                isMyBooksOpen, setIsMyBooksOpen, url, isLoading, chosenCurrentlyBook, setChosenCurrentlyBook
             }}
         >
             {children}
