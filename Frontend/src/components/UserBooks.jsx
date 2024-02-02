f99078e4d10daaadaf7afc6877e910ff37811d7a
DarynaBb <darinabilenko@gmail.com>
2024-02-02 17:43:21 +0100

    styling for currently-books-section

--- Frontend/src/components/UserBooks.jsx
+++ Frontend/src/components/UserBooks.jsx
@@ -1,6 +1,7 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { UserBooksContext } from '../context/UserBooksContext';
 import star from "../assets/star24Px.svg"
+import arrow from "../assets/arrowForward.svg"
 
 function UserBooks() {
   const {
@@ -18,75 +19,106 @@ function UserBooks() {
     isBookDeleted,
     changeShelf,
     deleteBook,
-    isMyBooksOpen
+    isMyBooksOpen, isLoading, chosenCurrentlyBook, setChosenCurrentlyBook
   } = useContext(UserBooksContext);
-
+  
   useEffect(() => {
     getBooks();
   }, [isShelfUpdated, isBookDeleted]);
 
-  const renderBookList = (books, shelfType) => (
-    <ul className=''>
-  <li>
-    <p className='pt-regular text-[32px] mb-[24px]'>{shelfType} ({books.length} {books.length !== 1 ? "books" : "book"})</p>
-  </li>
-  {books.map((book, index) => (
-    index === 0 ? (
-      <li key={index} className='mb-[45px]'>
-        <div className='flex justify-between'>
-          <img className='max-w-[286px]' src={book.book.image} alt="" />
-          <div className='flex flex-col justify-between'>
-            <div>
-              <p className='inter-semi-bold text-[20px]'>{book.book.title}</p>
-              <p>by {book.book.author}</p>
-              <p className='pt-regular'>{book.book.description}</p>
-            </div>
-            <div className='flex gap-[10px]'>
-              <select className='bg-black text-white py-[20px] px-[50px]' onChange={(e) => changeShelf(e, book.book._id)} name="" id="" defaultValue={book.shelfType}>
-                <option value="currentlyReading">Currently Reading</option>
-                <option value="wantToRead">Want To Read</option>
-                <option value="read">Read</option>
-              </select>
-              <button className='bg-black text-white py-[20px] px-[75px]' onClick={() => deleteBook(book.book._id)}>Delete</button>
-            </div>
-          </div>
-        </div>
-        <div className='flex mt-[20px] gap-[5px] items-center'>
-            <p>{book.book.averageRating}</p>
-            <img src={star} alt="" />
-          </div>
-      </li>
-    ) : (
-      <li key={book.book._id} className='flex mb-[20px] gap-[20px]'>
-        <img className='max-w-[160px]' src={book.book.image} alt="" />
-        <div className='flex flex-col gap-[10px]'>
-        <p>{book.book.title}</p>
-        <p>{book.book.author}</p>
-        <p>{book.book.description}</p>
-        <select onChange={(e) => changeShelf(e, book.book._id)} name="" id="" defaultValue={book.shelfType}>
-          <option value="currentlyReading">Currently Reading</option>
-          <option value="wantToRead">Want To Read</option>
-          <option value="read">Read</option>
-        </select>
-        <button onClick={() => deleteBook(book.book._id)}>Delete</button>
-        </div>
-        
-      </li>
-    )
-  ))}
-</ul>
+  useEffect(() => {
+    if (isLoading && currentlyReading.length > 0) {
+      const book = currentlyReading.filter((book, index) => index === 0);
+      setChosenCurrentlyBook(book);
+      console.log("BOOK", chosenCurrentlyBook)
+    }  
+  },[])
 
-  );
+  const slideLeft = () => {
+    var slider = document.getElementById('slider');
+    slider.scrollLeft = slider.scrollLeft - 500;
+  };
+
+  const slideRight = () => {
+    var slider = document.getElementById('slider');
+    slider.scrollLeft = slider.scrollLeft + 500;
+  };
+
+  const onClickHandler = (title) => {
+    const book = currentlyReading.filter((book) => book.book.title === title);
+    setChosenCurrentlyBook(book);
+  };
 
   return (
     <section className=''>
-      <h2 className={isMyBooksOpen ? "block pt-regular text-[32px]" : "hidden"}>My Books</h2>
-      <div className='mt-[45px]'>
-        {isCurrentlyReading && renderBookList(currentlyReading, "Currently Reading")}
-        {isWantToRead && renderBookList(wantToRead, "Want To Read")}
-        {isRead && renderBookList(read, "Read")}
-      </div>
-    </section>
+      <h2 className={isMyBooksOpen ? "block pt-regular text-[32px] mb-[24px]" : "hidden"}>My Books</h2>
+
+      {isCurrentlyReading ? (
+        <>
+        {chosenCurrentlyBook?.map(book => (
+          <div key={book.book._id} className='mb-[45px]'>
+          <div className='flex justify-between'>
+            <img className='max-w-[286px]' src={book.book.image} alt="" />
+            <div className='flex flex-col justify-between'>
+              <div>
+                <p className='inter-semi-bold text-[20px]'>{book.book.title}</p>
+              <p>by {book.book.author}</p>
+              <p className='pt-regular mt-[20px]'>{book.book.description}</p>
+            </div>
+            <div className='flex justify-between gap-[20px]'>
+              <div className='flex items-start gap-[5px]'>
+                <p className='inter-semi-bold text-[14px]'>Move to:</p>
+                <select className='text-[14px] outline-bg-gray' onChange={(e) => changeShelf(e, book.book._id)} name="" id="" defaultValue={book.shelfType}>
+                  <option value="currentlyReading">Currently Reading</option>
+                  <option value="wantToRead">Want To Read</option>
+                  <option value="read">Read</option>
+                </select>
+              </div>
+              <button className='bg-black text-white py-[20px] px-[75px]' onClick={() => deleteBook(book.book._id)}>Delete book</button>
+           </div>
+          </div>
+          </div>
+          <div className='flex mt-[20px] justify-between items-center max-w-[286px]'>
+            <div className='flex gap-[5px] items-center'>
+              <p>{book.book.averageRating}</p>
+              <img src={star} className='w-[20px]' alt="" />
+            </div>
+            <p>58%</p>
+          </div>
+          </div> 
+        ))}   
+        <div className='flex justify-between items-center'>
+          <div>
+            <img src={arrow} className='rotate-180 opacity-50 cursor-pointer hover:opacity-100' onClick={slideLeft} alt="" />
+          </div>
+          <ul id='slider' className='flex gap-[20px] max-w-[700px] h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth scrollbar-hide '>
+            {currentlyReading?.map((book, index) => (
+              <li onClick={() => onClickHandler(book.book.title)} key={book.book._id} className='flex flex-col gap-[20px] cursor-pointer '>
+                <img className='max-w-[160px]  h-full object-cover hover:-translate-y-3 ease-in-out duration-500' src={book.book.image} alt="" />
+                <div className='flex mt-[10px] justify-between items-center'>
+                  <div className='flex gap-[5px] items-center'>
+                    <p>{book.book.averageRating}</p>
+                    <img src={star} className='w-[20px]' alt="" />
+                  </div>
+                  <p>58%</p>
+                </div>
+              </li>
+            ))}
+          </ul>
+        <div>
+          <img src={arrow} className='opacity-50 cursor-pointer hover:opacity-100' onClick={slideRight} alt="" />
+        </div>
+      </div> 
+    </>
+    ) : ("")}
+  </section>
+
+          
+      
+      
+
+      
+   
   );
 }
 
